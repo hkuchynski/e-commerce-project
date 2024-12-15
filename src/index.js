553726df69ee98cc9f3defc95dcea59adc94685a
Hanna Kuchynski <hannakuchynski@Hannas-MBP.fios-router.home>
2024-12-15 13:41:42 -0500

    added cart context, shopping cart

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
-import { UserProvider, userProvider } from './contexts/user.context';
+import { UserProvider } from './contexts/user.context';
+import { CartProvider} from './contexts/cart.context';
 
 import './index.scss';
 
@@ -12,7 +13,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <CartProvider>
+          <App />
+        </CartProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
