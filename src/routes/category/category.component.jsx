c099f2de4c219e45159bfb5daad7acd312ce1f9c
Hanna Kuchynski <hannakuchynski@Hannas-MBP.fios-router.home>
2024-12-18 12:50:41 -0500

    updated selecotors with memoization

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -10,10 +10,13 @@ import './category.styles.scss';
 
 const Category = () => {
     const { category } = useParams();
+    console.log('render/re-rendering category component');
     const categoriesMap = useSelector(selectCategoriesMap);
     const [products, setProducts] = useState(categoriesMap[category]);
+    
 
     useEffect(() => {
+        console.log('effect fired calling setProducts')
         setProducts(categoriesMap[category]);
     }, [category, categoriesMap]);
 
